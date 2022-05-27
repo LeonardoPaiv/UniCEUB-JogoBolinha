f973cd3b0f9a2821ef4ca53eb1548f636bb79ce8
leonardoPaiv <leopaiva45@gmail.com>
2022-05-27 16:32:17 -0300

    adicionado final de jogo 10 pt e som de rebatida

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from pyparsing import line
 from graphics import *
 import random
 import time
+from playsound import playsound
+import os
+import threading
 
 win = GraphWin("Bolinha ...", 1600, 600)
 
@@ -68,7 +71,6 @@ while continuar:
         if random.random() < 0.5:
             passo = -passo
         bateu = False
-    print('posições: ',col)
     #Ponto para o player 1
     if (col + raio + passo) > 1650:
         passo = -passo
@@ -118,10 +120,32 @@ while continuar:
     # Colisão com a barra acontece quando a bolhinha está na coluna 20 quando a velocidade lateral é 30
     if col == 20 and lin > linha_Barra_Esquerda and lin < (linha_Barra_Esquerda + 100):
         passo = - passo
+        
+        # Plays sound when ball hits bar
+        sound_file_path = os.path.join(
+            os.getcwd(),
+            "ball_hit_bar.wav"
+        )
+        threading.Thread(
+            target=playsound,
+            args=(sound_file_path,),
+            daemon=True
+            ).start()
 
     # Colisão com a barra acontece quando a bolhinha está na coluna 1580 quando a velocidade lateral é 30
     if col == 1580 and lin > linha_Barra_Direita and lin < (linha_Barra_Direita + 100):
         passo = - passo
+
+        # Plays sound when ball hits bar
+        sound_file_path = os.path.join(
+            os.getcwd(),
+            "ball_hit_bar.wav"
+        )
+        threading.Thread(
+            target=playsound,
+            args=(sound_file_path,),
+            daemon=True
+            ).start()
 
     # Nova posição do círculo
     circulo.undraw()
@@ -133,11 +157,6 @@ while continuar:
 
     # Movimento horizontal da barra pelas setas direita/esquerda
     tecla = win.checkKey()
-
-    # Sair do joguinho
-    if tecla == "Escape":
-        continuar = False
-        continue
 
     # movimento da barra esquerda
     if tecla == "W" or tecla ==  "w":
@@ -183,8 +202,45 @@ while continuar:
         barra_Direita.setWidth(10)
         barra_Direita.draw(win)
 
+
+    # Sair do joguinho
+    if tecla == "Escape":
+        continuar = False
+        continue
+
+    if pontos01 == 10 or pontos02 == 10:
+        continuar = False
+        final_do_jogo = True
+        continue
+
     
     # Esperar o ser humano reagir
-    time.sleep(.07)
+    time.sleep(0.04)
+
+while final_do_jogo:
+    barra_Direita.undraw()
+    barra_Esquerda.undraw()
+    circulo.undraw()
+
+    tecla = win.checkKey()
+
+    if pontos01 == 10:
+        placar.undraw()
+        vitoria = Text(Point(800,300), '''Jogador numero 1 venceu
+pressione esc para sair''')
+        vitoria.setSize(20)
+        vitoria.draw(win)
+
+    if pontos02 == 10:
+        placar.undraw()
+        vitoria = Text(Point(800,300), '''Jogador numero 2 venceu
+pressione esc para sair''')
+        vitoria.setSize(20)
+        vitoria.draw(win)
+        
+    
+    if tecla == "Escape":
+        final_do_jogo = False
+        continue
 
 win.close()
