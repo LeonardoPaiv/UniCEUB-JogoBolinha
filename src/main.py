011eaeb5b5e4a79dacf3854c766af5c8a53ccd90
PedroHBrasil <pedrohbrasil7@gmail.com>
2022-06-23 22:25:35 -0300

    Added main file

---
+++ src/main.py
@@ -0,0 +1,39 @@
+
+
+import tkinter as tk
+from graphics import GraphWin
+from src.jogo.jogo import Jogo
+from src.menu.menu import Menu
+from src.ranking.ranking import Ranking
+
+
+def rodar():
+
+    # Encontra a resolução da tela
+    root = tk.Tk()
+    tela_largura = root.winfo_screenwidth()
+    tela_altura = root.winfo_screenheight()
+    root.destroy()
+
+    # Inicializa janela do jogo
+    janela_largura = tela_largura/2
+    janela_altura = tela_altura/2
+    janela = GraphWin("Nosso Pong", janela_largura, janela_altura)
+
+    # Loop do programa
+    sair = False
+    while not sair:
+        menu = Menu()
+        menu.rodar()
+        if menu.iniciar:
+            jogo = Jogo()
+            sair = jogo.rodar(janela)
+        elif menu.ranking:
+            ranking = Ranking()
+            ranking.rodar(janela)
+        elif menu.sair:
+            sair = True
+
+
+if __name__ == "__main__":
+    rodar()
